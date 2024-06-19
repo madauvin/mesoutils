ecabe53e79a25aed9c21d5dd28420ec738a4f548
Magali Dauvin <magali.dauvin@sciencespo.fr>
2024-06-19 14:18:42 +0000

    Création de documentation pour les données

---
+++ R/mydataset.R
@@ -0,0 +1,9 @@
+#'Starwars data, only Human species
+#'
+#' A subset of data from the World Health Organization Global Tuberculosis
+#' Report ...
+#' @importFrom dplyr starwars
+#' @format ## `mydataset`
+#' A tibble with 35 rows and 14 columns
+#' @source <https://dplyr.tidyverse.org/reference/starwars.html>
+"mydataset"
